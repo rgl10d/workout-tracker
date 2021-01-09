3b08b359bb8ae4202adf897eb72cafc4bd471b6a
Garrett Lee <rgl10d@gmail.com>
2021-01-09 14:38:08 -0500

    added post api route and connected them to server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,31 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  // GET ROUTE
+  // app.get("/api/workouts", function (req, res) {
+  //   db.Workout.find({})
+  //   .then(function (dbWorkout) {
+  //     res.json(dbWorkout);
+  //   });
+  // });
 
+  // CREATE NEW WORKOUTS
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  // PUT ROUTE
+
+  // app.get("/api/workouts/:id", function (req, res) {
+  //   db.Workout.findByIdAndUpdate(req.params.id)
+  //   .then(function (dbWorkout) {
+  //     res.json(dbWorkout);
+  //   });
+  // });
 };
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 
+// Express instance
 const app = express();
-const db = require('./models/')
 
 // Define PORT
 const PORT = process.env.PORT || 3000;
@@ -16,6 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Connect routes
+app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
+
 // Defining a connection to Mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", {
     useNewUrlParser: true,
@@ -25,15 +29,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", {
   });
 
 // Post request to the database
-app.post("/submit", ({ body }, res) => {
-  db.Resistance.create(body)
-    .then(dbExercise => {
-      res.json(dbExercise);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+
 
 // Listen to the PORT
 app.listen(PORT, () => {
