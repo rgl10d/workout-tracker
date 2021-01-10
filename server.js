92f7cb54de788c29420c4d47e6a209b03d88bf91
Garrett Lee <rgl10d@gmail.com>
2021-01-09 23:05:33 -0500

    added mongoose connection check on server.js

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useCreateIndex: true,
 });
 
+connection.on("connected", () => {
+  console.log("Mongoose connected successfully.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: " + err);
+});
+
 // Listen to PORT
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
